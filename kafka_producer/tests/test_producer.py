44577dd0008dd10862c8033e615ef8381d4556f6
Taher <tahervali@gmail.com>
2024-09-10 18:31:37 +0530

    Basic commit for tests con

---
+++ kafka_producer/tests/test_producer.py
@@ -0,0 +1,19 @@
+# import pytest
+
+# # import json
+# from unittest.mock import patch
+
+# # from kafka.errors import KafkaError
+# # from kafka_producer import CustomKafkaProducer, MAX_RETRIES, RETRY_DELAY, TOPIC_NAME
+
+
+# @pytest.fixture
+# def mock_kafka_producer():
+#     with patch("your_module.KafkaProducer") as mock:
+#         yield mock
+
+
+# @pytest.fixture
+# def mock_logger():
+#     with patch("your_module.logger") as mock:
+#         yield mock
